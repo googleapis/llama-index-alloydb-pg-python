7edfd0da6d1b9c4ef5b01a41d7636477f0f370e2
Disha Prakash <dishaprakash@google.com>
2024-11-27 11:22:22 +0000

    feat: Adding AlloyDBDocumentStore

--- src/llama_index_alloydb_pg/__init__.py
+++ src/llama_index_alloydb_pg/__init__.py
@@ -13,5 +13,6 @@
 # limitations under the License.
 
 from .engine import AlloyDBEngine, Column
+from .document_store import AlloyDBDocumentStore
 
-_all = ["AlloyDBEngine", "Column"]
+_all = ["AlloyDBEngine", "Column", AlloyDBDocumentStore]
