21f06e0d7e52c35f267281102ddec94183e08c7d
Disha Prakash <dishaprakash@google.com>
2024-11-27 11:25:58 +0000

    Linter fix.

--- src/llama_index_alloydb_pg/__init__.py
+++ src/llama_index_alloydb_pg/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .engine import AlloyDBEngine, Column
 from .document_store import AlloyDBDocumentStore
+from .engine import AlloyDBEngine, Column
 
 _all = ["AlloyDBEngine", "Column", AlloyDBDocumentStore]
