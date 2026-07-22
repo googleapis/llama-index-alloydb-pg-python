8119db64bc33cedfd9ebdc89fd81df06f39c02b2
Pavan Madamsetty <pmsetty@google.com>
2026-07-22 04:40:33 +0000

    fix(indexes): Set DistanceStrategy.INNER_PRODUCT scann index function to dot_product as verified on live AlloyDB cluster

--- src/llama_index_alloydb_pg/indexes.py
+++ src/llama_index_alloydb_pg/indexes.py
@@ -32,7 +32,7 @@ class DistanceStrategy(StrategyMixin, enum.Enum):
 
     EUCLIDEAN = "<->", "l2_distance", "vector_l2_ops", "l2"
     COSINE_DISTANCE = "<=>", "cosine_distance", "vector_cosine_ops", "cosine"
-    INNER_PRODUCT = "<#>", "inner_product", "vector_ip_ops", "dot_prod"
+    INNER_PRODUCT = "<#>", "inner_product", "vector_ip_ops", "dot_product"
 
 
 DEFAULT_DISTANCE_STRATEGY: DistanceStrategy = DistanceStrategy.COSINE_DISTANCE
